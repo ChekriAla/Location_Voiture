7ce45b342d31d73bee171c7995ed72b922f3c209
HajriAbderrahim <hajri.rahim@hotmail.com>
2017-05-02 23:36:46 +0200

    Update List_par_prix.java

--- app/src/main/java/com/example/aio_5/location_voiture/List_par_prix.java
+++ app/src/main/java/com/example/aio_5/location_voiture/List_par_prix.java
@@ -1,17 +1,159 @@
 package com.example.aio_5.location_voiture;
 
-
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
 
 
 public class List_par_prix extends AppCompatActivity {
     
+    private RecyclerView recyclerView;
+    private GridLayoutManager gridLayoutManager;
+    private CustomAdapter adapter;
+    private List<MyData> data_list;
+    public Integer x;
+    RequestQueue requestQueue;
 
+    String showUrl = "http://10.0.2.2/rent/showCars.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_par_prix);
+        
+    
+        
+        
+        TextView textView=(TextView) findViewById(R.id.txt_bundle);
+        Bundle bundle=getIntent().getExtras();
+        final String data=bundle.get("data").toString();
+        textView.setText("Prix "+data);
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(com.android.volley.Request.Method.POST,
+                showUrl, null,new com.android.volley.Response.Listener<JSONObject>() {
+
+            public void onResponse(JSONObject response) {
+                System.out.println(response.toString());
+                try {
+                    JSONArray cars = response.getJSONArray("cars");
+
+                    for (int i = 0; i < cars.length(); i++) {
+                        JSONObject car = cars.getJSONObject(i);
+
+
+                        String prix = car.getString("prix");
+
+                        String s = String.valueOf(x);
+                        if( prix.equals(data)){
+
+                            x = car.getInt("id");
+
+                            load_data_from_server(x-1);
+
+                        }
+                    }
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+
+            public void onErrorResponse(VolleyError error) {
+                System.out.append(error.getMessage());
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        data_list  = new ArrayList<>();
+
+        gridLayoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        adapter = new CustomAdapter(this,data_list);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+                if(gridLayoutManager.findLastCompletelyVisibleItemPosition() == data_list.size()-1){
+
+                }
+
+            }
+        });
+
+    }
+
+    private void load_data_from_server(int id) {
+
+        AsyncTask<Integer,Void, Void> task = new AsyncTask<Integer, Void, Void>(){
+            @Override
+            protected Void doInBackground(Integer... integers) {
+
+                OkHttpClient client = new OkHttpClient();
+
+                okhttp3.Request request = new okhttp3.Request.Builder()
+                        .url("http://192.168.100.100/rent/script.php?id="+integers[0])
+                        .build();
+                try {
+                    okhttp3.Response response = client.newCall(request).execute();
+
+                    JSONArray array = new JSONArray(response.body().string());
+
+
+                    JSONObject object = array.getJSONObject(0);
+                    MyData data = new MyData(object.getInt("id"),object.getString("description"),
+                            object.getString("image"));
+
+                    data_list.add(data);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    System.out.println("End of content");
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+        task.execute(id);
+
+
 
     }
 
